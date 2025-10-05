fbf2341612d3f07f4bdc6fe139e6504950cb8610
Anthony <isijolaayomikun04@gmail.com>
2025-10-05 10:59:18 -0700

    second commit

--- scripts/toggle.js
+++ scripts/toggle.js
@@ -1,12 +1,26 @@
 const toggleButton = document.querySelector('.toggle-button');
 const themeIcon = document.querySelector('.theme-icon');
+const savedTheme = localStorage.getItem('theme');
+
+if (savedTheme) {
+  document.body.classList.toggle('dark', savedTheme === 'dark');
+  themeIcon.src = savedTheme === 'dark' ? 'images/sun-color-icon.svg' : 'images/night-icon.svg';
+} else {
+  if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
+    document.body.classList.add('dark');
+    themeIcon.src = 'images/sun-color-icon.svg';
+  } else {
+    themeIcon.src = 'images/night-icon.svg';
+  }
+}
 
 toggleButton.addEventListener('click', () => {
   document.body.classList.toggle('dark');
 
   if (document.body.classList.contains('dark')) {
-    themeIcon.src = 'images/6165787.png';
+    themeIcon.src = 'images/sun-color-icon.svg';
   } else {
-    themeIcon.src = 'images/moon_891869.png';
+    themeIcon.src = 'images/night-icon.svg';
+    localStorage.setItem('theme', 'light');
   }
 });
